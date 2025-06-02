291b480c38653542b6ddb7507ea2c553362c1575
Tales Augusto Siqueira <117324782+eutales@users.noreply.github.com>
2025-06-02 18:51:05 -0300

    Create pasta sonia.sql
    .

---
+++ docs_do_projeto/Banco_de_dados/SQL/pasta sonia.sql
@@ -0,0 +1,12 @@
+use biblioteca;
+Select * from livro;
+from * Cliente c
+join multa.m ou c. id.cliente=m.id.cliente
+
+drop table livro;
+drop table emprestimo;
+drop table cliente;
+drop table multa;
+select * from
+
+
